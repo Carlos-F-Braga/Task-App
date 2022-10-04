2f6bc4a04c694db69bbc2f91a023e5aa82d285de
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-04 19:33:53 -0300

    await playground ;-; all commits gone

---
+++ playground/9.1-await.js
@@ -0,0 +1,19 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+const doWork = async () => {
+    const sum = await add(1, 99)
+    const sum2 = await add(sum, 9249)
+    const sum3 = await add(sum2, 9545339)
+    const sum4 = await add(sum3, 9643669)
+    return sum4
+}
+
+doWork().then((result) => {
+    console.log(result)
+})

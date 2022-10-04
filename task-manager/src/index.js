8bc69fffdd626a079554bc19cf457ebf4f695fd1
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-04 20:52:09 -0300

    delete endpoints and challenge programmed

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -87,6 +87,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        
+        if (!user) {
+            return res.status(404).send('No user exists')
+        }
+
+        res.status(200).send(user)
+    } catch (e) {
+        return res.status(400).send(e)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -166,7 +180,19 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        
+        if (!task) {
+            return res.status(404).send('No task exists')
+        }
 
+        res.status(200).send(task)
+    } catch (e) {
+        return res.status(400).send(e)
+    }
+})
 
 app.listen(port, () => {
     console.log('Server is listening on port', port )

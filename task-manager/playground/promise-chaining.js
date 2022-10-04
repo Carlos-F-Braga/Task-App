f35f6db523fb397d8818cd4fd64f43a9e47f108b
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-04 20:07:08 -0300

    async await in the promise mongoose playground

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,11 +1,21 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('632e5413b2464c1f2c0bdf68', { age: 1 }).then((user) => {
-    console.log('user', user)
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
-    console.log('count', result)
-}).catch((err) => {
-    console.log(err)
+// User.findByIdAndUpdate('632e5413b2464c1f2c0bdf68', { age: 1 }).then((user) => {
+//     console.log('user', user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log('count', result)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('632e5496c5bc19327cad8737', 27).then((count) => {
+    console.log('count', count)
 })

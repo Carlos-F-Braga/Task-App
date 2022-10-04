e203c96d970ee3f226f6790cc0109f579dde2dc8
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-04 20:07:22 -0300

    promise 2 challenge programmed with async await

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,21 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('632e720d5c73ac21d4532c32').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log('count', result)
-}).catch((err) => {
-    console.log(err)
+// Task.findByIdAndDelete('632e720d5c73ac21d4532c32').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log('count', result)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndUpdate(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('633cbbf64c3ffd29d06abf42').then((count) => {
+    console.log('count', count)
 })

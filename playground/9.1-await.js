e9fa42d19be9d6628fa8669516fd3e4a1cc1e150
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-04 19:36:55 -0300

    error handling await async

--- playground/9.1-await.js
+++ playground/9.1-await.js
@@ -1,6 +1,9 @@
 const add = (a, b) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            if (a < 0 || b < 0)
+            return reject('Numbers must be positive')
+
             resolve(a + b)
         }, 2000)
     })

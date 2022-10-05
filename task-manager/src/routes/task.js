c3179503d1c71f112ebc8ca9fe27375387b5d99d
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-04 21:23:42 -0300

    middleware accepting code for tasks

--- task-manager/src/routes/task.js
+++ task-manager/src/routes/task.js
@@ -69,7 +69,13 @@ router.patch('/tasks/:id', async (req, res) => {
         return res.status(400).send({error: 'Invalid updates!'})
 
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, })
+        const task = await Task.findById(req.params.id)
+
+        updates.forEach((update) => task[update] = req.body[update])
+
+        await task.save()
+
+        // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, })
         
         if (!task) {
             return res.status(404).send('No task exists')

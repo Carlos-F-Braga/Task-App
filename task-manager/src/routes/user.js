631f8940291ee857c3be0cab34b817e9c99a7d45
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-04 21:07:38 -0300

    ocd

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-    
+
     try {
         await user.save()
         res.status(201).send(user)

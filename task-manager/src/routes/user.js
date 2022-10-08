846bbbb2f3323b547f6fac55e02ea0447ff76d47
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-08 09:46:17 -0300

    Error handling

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -196,6 +196,8 @@ router.delete('/users/:id', async (req, res) => {
 
 router.post('/users/me/avatar', upload.single('avatar'),  (req, res) => {
     res.send()
+  }, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
   })
 
 

27ba30eededb86f6e53947696665be8b986d2323
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-07 21:51:04 -0300

    authentication integrated

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = new express.Router()
+const auth = require('../middleware/auth')
 const User = require('../models/user')
 
 router.post('/users', async (req, res) => {
@@ -30,7 +31,7 @@ try {
 }
 })
 
-router.get('/users', async (req, res) => {
+router.get('/users', auth, async (req, res) => {
 
     try {
         const users = await User.find({})

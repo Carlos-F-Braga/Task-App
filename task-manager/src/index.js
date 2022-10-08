06f0bb19b1176ecaf14f72b21977bfb7891cb740
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-08 09:42:28 -0300

    upload playground fileFilter

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -71,7 +71,16 @@ app.listen(port, () => {
 
 const multer = require('multer')
 const upload = multer({
-  dest: 'images'
+  dest: 'images',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+        return cb(new Error('File must be a Doc'))
+    }
+    cb(undefined, true)
+  }
 })
 
 app.post('/upload', upload.single('upload'),  (req, res) => {

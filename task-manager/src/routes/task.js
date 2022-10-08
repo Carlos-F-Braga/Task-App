0328f7aa82243e6e8af97cb4905b1f62d600d81e
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-08 00:28:11 -0300

    find task by id or logged in user programmed

--- task-manager/src/routes/task.js
+++ task-manager/src/routes/task.js
@@ -40,15 +40,16 @@ router.get('/tasks', async (req, res) => {
     // })
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
 
-    const task = await Task.findById(_id)
-
     try {
+        const task = await Task.findOne({ _id, owner: req.user._id})
+
         if (!task) {
             return res.status(404).send()
         }
+        
         res.status(200).send(task)
     } catch (e) {
         return res.status(500).send(e)

d4932e3df1dd3f2ff4b920c7da93f10fd32c836d
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-07 23:38:26 -0300

    swagger tags programmed

---
+++ task-manager/src/swagger/-tags.js
@@ -0,0 +1,12 @@
+/**
+ * @swagger
+ * tags:
+ *   -  name: User.js
+ *      description: Requests de users da API
+ * 
+ *   -  name: Task.js
+ *      description: Requests de tasks da API
+ * 
+ *  
+ * 
+ * */

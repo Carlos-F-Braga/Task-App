a1c13b13b5dde7d13d1b2ecf858fa6b7e6ac19e7
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-09 00:37:01 -0300

    post users

--- task-manager/src/swagger/user.js
+++ task-manager/src/swagger/user.js
@@ -1,5 +1,22 @@
 /**
  * @swagger
+ * 
+ * /usuarios/login:
+ *  post:
+ *    tags:
+ *    - "User.js"
+ *    summary: "Rota de criação do Usuário"
+ *    description: Realização do post referente a criação do Usuário
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: "#/components/schemas/SignUp"
+ *    responses:
+ *      '200':
+ *        description: Sucesso
+ * 
+ * 
  * /users/me:
  *  get:
  *    tags:

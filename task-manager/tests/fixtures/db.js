a96fc7541591853b9a8af6d608aed2ed26ca084e
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-09 13:49:03 -0300

    userTwo programmed

--- task-manager/tests/fixtures/db.js
+++ task-manager/tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 const userOneId = new mongoose.Types.ObjectId
 const userOne =  {
@@ -13,9 +14,21 @@ const userOne =  {
     }]
 }
 
+const userTwoId = new mongoose.Types.ObjectId
+const userTwo =  {
+    _id: userTwoId,
+    name: 'Andro',
+    email: 'Andro@m3ke.com',
+    password: '1234567',
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
+    }]
+}
+
 const setupDatabase = async () => {
     await User.deleteMany()
     await new User(userOne).save()
+    await new User(userTwo).save()
 }
 
 module.exports = {

ef8b34f655b14399031c7eebb3950e5d06cca45c
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-08 23:52:04 -0300

    swagger programming started

---
+++ task-manager/src/swagger/task.js
@@ -0,0 +1,19 @@
+/**
+ * @swagger
+ * 
+ * 
+ * /users/me:
+ *  get:
+ *    tags:
+ *    - "User.js"
+ *    summary: Rota de get das Cidades de um Estado
+ *    security:
+ *    - bearerAuth: []
+ *    description: Realiza um get recebendo todos as cidades em um especifico estado
+ *    responses:
+ *      '200':
+ *        description: Sucesso
+ *      '500':
+ *        description: Erro * 
+ * 
+ */

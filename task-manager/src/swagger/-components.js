379dd2b4bd3d41a9cb3777b987a923f9141745c1
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-09 00:36:51 -0300

    components initially programmed

---
+++ task-manager/src/swagger/-components.js
@@ -0,0 +1,29 @@
+
+/**
+ *@swagger
+ *
+ * components:
+ *   schemas:
+ *    SignUp:
+ *      type: object
+ *      properties:
+ *        name:
+ *          type: "string"
+ *          example: Cintia2
+ *        email:
+ *          type: "string"
+ *          example: "425262"
+ *    Login:
+ *      type: object
+ *      properties:
+ *        usuario:
+ *          type: "string"
+ *          example: "Carls Meader"
+ *        email:
+ *          type: "string"
+ *          example: "kap@jai.com"
+ *        senha:
+ *          type: "string"
+ *          example: "1234567"
+ * 
+ */

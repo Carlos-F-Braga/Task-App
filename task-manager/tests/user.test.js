596c50d95e1fd6e5ed3fea7f0770c4efb232e388
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-09 12:11:49 -0300

    assynchronism testing with beforeEach

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,2 +1,16 @@
 const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/models/user')
 
+
+beforeEach(async () => {
+    await User.deleteMany()
+})
+
+test('should sign up a new user', async () => {
+    await request(app).post('/users').send({
+        name: 'Carlos',
+        email: 'Carlois@egeru.com.br',
+        password: '123478593'
+    }).expect(201)
+})
